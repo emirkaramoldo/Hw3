b11cc2820a9403a231d9ac4d84a4ab5290b93dba
emirkaramoldoev <emirkaramoldoev1@gmail.com>
2023-05-05 23:11:49 +0600

    Перебор массива и условие отбора положительных чисел

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        double[] numbers = {3,-6, 9, -12, 15, -18, 21, -24, 27, -30, 33, -36, 39, -42, 45};
+        double sum = 0;
+        int amount = 0;
+        boolean isNegative = false;
+        for (double num:numbers
+             ) {
+            if (num < 0){
+            isNegative = true;
+            continue;
+            }
+            else if (!(num > 0 && isNegative));{
+                sum += num;
+                amount ++;
+            }
         }
+        System.out.println(sum);
+        System.out.println(amount/sum);
     }
 }
+
